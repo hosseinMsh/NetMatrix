d9e79339ed840d47043a06bf5e84f866f3783446
Iliya <iliyaforsati2005@gmail.com>
2025-03-02 18:34:41 +0330

    static files

--- NetMatrix/settings.py
+++ NetMatrix/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,9 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# settings.py
 
-# Default primary key field type
+# Define the directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Define the URL prefix for static files
+
+# Additional locations for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Example: A global static directory
+]# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
