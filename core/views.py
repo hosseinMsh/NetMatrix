a0b67f8b896fc4aaf72b62e2a029cc1ea5abb521
Iliya <iliyaforsati2005@gmail.com>
2025-03-02 18:33:57 +0330

    add view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'GET':
+        return render(request, "main.html")
+    if request.method == 'POST':
+        request.POST.get('start_ip')
+        return HttpResponse("response")
