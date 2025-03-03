995ba4abe90104a373f58aa748eeab8ada97218b
Hossein <hossein.masihi@gmail.com>
2025-03-03 08:23:20 +0330

    remove comment

--- core/api/ping.py
+++ core/api/ping.py
@@ -3,25 +3,17 @@ import re
 
 
 def ping_ip(ip_address, packet_count=4):
-    """
-    پینگ گرفتن از یک آدرس IP مشخص شده و نمایش نتیجه
-    :param ip_address: آدرس IP که باید پینگ شود
-    :param packet_count: تعداد پینگ‌ها (پیش‌فرض 4)
-    """
     try:
-        # اجرای دستور ping
         result = subprocess.run([
             "ping", "-c", str(packet_count), ip_address
         ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         output = result.stdout
 
-        # بررسی اینکه آیا پینگ موفق بوده است یا خیر
         if "100% packet loss" in output:
             print(f"\033[91mIP address {ip_address} is not reachable.\033[0m")
             return False
 
-        # استخراج مقدار میانگین پینگ از خروجی
         match = re.search(r'min/avg/max/mdev = ([\d.]+)/([\d.]+)/([\d.]+)/([\d.]+)', output)
         if match:
             avg_ping = match.group(2)
@@ -36,7 +28,6 @@ def ping_ip(ip_address, packet_count=4):
         return False
 
 
-# نمونه استفاده
 if __name__ == "__main__":
     ip = "8.8.8.8"  # آدرس IP برای تست
     ping_ip(ip)
